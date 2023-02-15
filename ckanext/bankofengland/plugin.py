3428a5d4a84ac2cc1abeab86c8fe2282be002d27
Michael Polidori <contact@michaelpolidori.com>
2023-02-15 14:08:16 -0500

    Use title caps for market values; Add missing facet functions

--- ckanext/bankofengland/plugin.py
+++ ckanext/bankofengland/plugin.py
@@ -1,9 +1,15 @@
+import logging
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+log = logging.getLogger(__name__)
+
+
 class BankofenglandPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
 
     # IConfigurer
 
@@ -11,3 +17,13 @@ class BankofenglandPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'bankofengland')
+
+    def dataset_facets(self, facets_dict, package_type):
+        facets_dict['market_value_by'] = plugins.toolkit._('Market Value By')
+        return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        return facets_dict
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return facets_dict
