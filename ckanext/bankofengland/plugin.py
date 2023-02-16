95cd697ee98a4e480cc13cfe8c0c11fe71111ac9
Luccas Mateus <Luccasmmg@gmail.com>
2023-02-16 14:52:41 -0300

    [data-api][m] - create view route (#2)

--- ckanext/bankofengland/plugin.py
+++ ckanext/bankofengland/plugin.py
@@ -1,9 +1,12 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.bankofengland import actions
 
 class BankofenglandPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IConfigurable)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
 
@@ -11,3 +14,25 @@ class BankofenglandPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'bankofengland')
+
+    # IConfigurable
+
+    def configure(self, config):
+        # Certain config options must exists for the plugin to work. Raise an
+        # exception if they're missing.
+        missing_config = "{0} is not configured. Please amend your .ini file."
+        config_options = (
+            'ckanext.bankofengland.hasura_url',
+            'ckanext.bankofengland.hasura_admin_key',
+            'ckanext.bankofengland.hasura_db',
+        )
+        for option in config_options:
+            if not config.get(option, None):
+                raise RuntimeError(missing_config.format(option))
+    
+    #IActions
+    def get_actions(self):
+        return {
+            'create_view': actions.create_view
+        }
+
