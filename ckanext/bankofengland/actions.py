24cc98fd68a6f19c37dd61bb7b63e81e22c23af3
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-03-03 14:11:41 -0300

    [history][m] - added history route

--- ckanext/bankofengland/actions.py
+++ ckanext/bankofengland/actions.py
@@ -16,7 +16,23 @@ def build_joins(input):
 
 
 def build_sql(input):
-    return "CREATE VIEW {table_name} AS SELECT {first_table}.\"Date\", {list_of_tables} FROM {first_table} {joins}".format(table_name = build_id(input), first_table=input[0], list_of_tables=", ".join(f'{x}."{x.upper()}"' for x in input), joins=build_joins(input))
+    return 'CREATE VIEW {table_name} AS SELECT {first_table}."Date", {list_of_tables} FROM {first_table} {joins}'.format(
+        table_name=build_id(input),
+        first_table=input[0],
+        list_of_tables=", ".join(f'{x}."{x.upper()}"' for x in input),
+        joins=build_joins(input),
+    )
+
+
+def build_sql_history(table_name, list_of_resources):
+    return 'CREATE VIEW {table_name} AS SELECT {first_table}."Date", {list_of_tables} FROM {first_table} {joins}'.format(
+        table_name=table_name + "_history",
+        first_table=list_of_resources[0],
+        list_of_tables=", ".join(
+            f'{x}."{table_name.upper()}" as {x}' for x in list_of_resources
+        ),
+        joins=build_joins(list_of_resources),
+    )
 
 
 def add_permissions(table_name):
@@ -68,6 +84,7 @@ def run_sql(query):
     response = requests.post(url, json=body, headers=headers)
     return response.status_code
 
+
 def create_view(context, data_dict):
     if "tables" not in data_dict:
         raise toolkit.ValidationError("Missing tables value in input")
@@ -80,40 +97,91 @@ def create_view(context, data_dict):
     track_view(build_id(data_dict["tables"]))
     return add_permissions(build_id(data_dict["tables"]))
 
+
+@toolkit.side_effect_free
+def get_history(context, data_dict):
+    package = toolkit.get_action("package_show")(
+        None, {"id": data_dict["package_id"].lower()}
+    )
+    resources_sorted = sorted(
+        package["resources"], key=lambda d: d["created"], reverse=True
+    )
+    run_sql(build_sql_history(
+        package["name"].lower(),
+        [resource["name"].lower() for resource in resources_sorted],
+    ))
+    track_view(package['name'].lower() + '_history')
+    add_permissions(package['name'].lower() + '_history')
+    return package['name'].lower() + '_history'
+
 @toolkit.side_effect_free
 def search_package_list(context, data_dict):
-    packages = toolkit.get_action('package_search')(None, { 'q': data_dict['q']})
-    return [package['name'] for package in packages['results']]
+    packages = toolkit.get_action("package_search")(None, {"q": data_dict["q"]})
+    return [package["name"] for package in packages["results"]]
+
 
 def get_related_tags(tag, base_terms, alias_terms):
-    base_term = list(filter(lambda term: term['BASE_TERM_UNSTEMMED'].lower() == tag.lower(), base_terms))
+    base_term = list(
+        filter(
+            lambda term: term["BASE_TERM_UNSTEMMED"].lower() == tag.lower(), base_terms
+        )
+    )
     if len(base_term) == 0:
         return [tag]
     else:
-        alias_terms = filter(lambda term: term['BASE_TERM_ID'] == base_term[0]['BASE_TERM_ID'] and term['INCLUDE_TERM'] != 'N ', alias_terms)
-        return [term['ALIAS_TERM_UNSTEMMED'] for term in alias_terms]
+        alias_terms = filter(
+            lambda term: term["BASE_TERM_ID"] == base_term[0]["BASE_TERM_ID"]
+            and term["INCLUDE_TERM"] != "N ",
+            alias_terms,
+        )
+        return [term["ALIAS_TERM_UNSTEMMED"] for term in alias_terms]
+
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
 
+
 @toolkit.chained_action
 def package_create(original_action, context, data_dict):
-    base_terms = json.load(open(str(pathlib.Path(__file__).parent.resolve()) + '/base_term_thesauros.json'))
-    alias_terms = json.load(open(str(pathlib.Path(__file__).parent.resolve()) + '/alias_term_thesauros.json'))
-    tags = data_dict['tags'] if 'tags' in data_dict else []
-    tags = [tag['name'] for tag in tags ]
-    new_tags = [{'name': tag_name, 'state': 'active' } for tag_name in flatten([get_related_tags(tag, base_terms, alias_terms) for tag in tags])]
-    data_dict['tags'] = new_tags
+    base_terms = json.load(
+        open(str(pathlib.Path(__file__).parent.resolve()) + "/base_term_thesauros.json")
+    )
+    alias_terms = json.load(
+        open(
+            str(pathlib.Path(__file__).parent.resolve()) + "/alias_term_thesauros.json"
+        )
+    )
+    tags = data_dict["tags"] if "tags" in data_dict else []
+    tags = [tag["name"] for tag in tags]
+    new_tags = [
+        {"name": tag_name, "state": "active"}
+        for tag_name in flatten(
+            [get_related_tags(tag, base_terms, alias_terms) for tag in tags]
+        )
+    ]
+    data_dict["tags"] = new_tags
     result = original_action(context, data_dict)
     return result
 
+
 @toolkit.chained_action
 def package_update(original_action, context, data_dict):
-    base_terms = json.load(open(str(pathlib.Path(__file__).parent.resolve()) + '/base_term_thesauros.json'))
-    alias_terms = json.load(open(str(pathlib.Path(__file__).parent.resolve()) + '/alias_term_thesauros.json'))
-    tags = data_dict['tags'] if 'tags' in data_dict else []
-    tags = [tag['name'] for tag in tags ]
-    new_tags = [{'name': tag_name, 'state': 'active' } for tag_name in flatten([get_related_tags(tag, base_terms, alias_terms) for tag in tags])]
-    data_dict['tags'] = new_tags
+    base_terms = json.load(
+        open(str(pathlib.Path(__file__).parent.resolve()) + "/base_term_thesauros.json")
+    )
+    alias_terms = json.load(
+        open(
+            str(pathlib.Path(__file__).parent.resolve()) + "/alias_term_thesauros.json"
+        )
+    )
+    tags = data_dict["tags"] if "tags" in data_dict else []
+    tags = [tag["name"] for tag in tags]
+    new_tags = [
+        {"name": tag_name, "state": "active"}
+        for tag_name in flatten(
+            [get_related_tags(tag, base_terms, alias_terms) for tag in tags]
+        )
+    ]
+    data_dict["tags"] = new_tags
     result = original_action(context, data_dict)
     return result
