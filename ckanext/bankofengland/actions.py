9e61e06b057e6106714875783a39e95f71c372e0
Michael Polidori <contact@michaelpolidori.com>
2023-03-24 14:41:40 -0400

    Fix Solr error when groups or tags are empty

--- ckanext/bankofengland/actions.py
+++ ckanext/bankofengland/actions.py
@@ -447,6 +447,9 @@ def get_related_datasets(context, data_dict):
         for item in items:
             names.append(item['name'])
 
+        if not names:
+            return query
+
         query += '{}{}:({})'.format(
             or_text, key, ' OR '.join(names)
         )
@@ -471,6 +474,8 @@ def get_related_datasets(context, data_dict):
                 query = _iterate_sub_dict_query(value, query)
         else:
             query += '{}{}:"{}"'.format(or_text, key, value)
+
+    log.error(query)
 
     results = toolkit.get_action('package_search')(
         context, {'q': query, 'rows': rows}
